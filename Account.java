9e8c75b35b64d28702317d50c1c8e1364eeca31d
AinfinitedZ <43518696+AinfinitedZ@users.noreply.github.com>
2021-09-24 00:51:00 -0400

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,23 @@
+public class Account
+{  
+    // field that store the account number of user
+    private String accountNumber;
+
+    // field that store the balance limit of user
+    private int balanceLimit;
+
+    //field that store the current balance of user
+    private int balanceCurrent;
+
+    // a constructor that create an instance with Account type
+    public void Account (String accountNumberInput, int balanceLimitInput)
+    {
+        this.accountNumber = accountNumberInput;
+        this.balanceLimit = balanceLimitInput;
+    }       
+
+    // 
+    public String getAccountNumber
+
+
+}
