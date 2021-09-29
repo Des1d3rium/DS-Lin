274b5c635d94a7a3951020df161148bfcfebf464
AinfinitedZ <43518696+AinfinitedZ@users.noreply.github.com>
2021-09-29 19:03:01 -0400

    Add files via upload

--- Account.java
+++ Account.java
@@ -6,18 +6,50 @@ public class Account
     // field that store the balance limit of user
     private int balanceLimit;
 
-    //field that store the current balance of user
-    private int balanceCurrent;
+    // field that store the current balance of user
+    private int balanceCurrent = 0;
 
     // a constructor that create an instance with Account type
-    public void Account (String accountNumberInput, int balanceLimitInput)
+    public Account (String accountNumberInput, int balanceLimitInput)
     {
         this.accountNumber = accountNumberInput;
         this.balanceLimit = balanceLimitInput;
     }       
 
-    // 
-    public String getAccountNumber
+    // a method that return one's account number 
+    public String getAccountNumber()
+    {
+        return this.accountNumber;
+    }
+
+    // a method that return one's current balance
+    public double getBalance()
+    {
+        return this.balanceCurrent;
+    }
 
 
+    // increase the current balance by input value
+    public void charge(double balanceIncrease)
+    {
+        this.balanceCurrent += balanceIncrease; 
+    }
+
+    // decrease the current balance by input value 
+    public void credit(double balanceDecrease)
+    {
+        this.balanceCurrent -= balanceDecrease;
+    }
+
+    // set the limitation of balance to be input value 
+    public void setBalanceLimit(int balanceLimit)
+    {
+        this.balanceLimit = balanceLimit;
+    }
+
+    // return the limit of balance of the account
+    public int getBalanceLimit()
+    {
+        return this.balanceLimit;
+    }
 }
